98143441359c98a2f9391fd0f9f660eb56a7db67
arturop <67555134+arturopstem@users.noreply.github.com>
2025-06-12 02:27:36 -0500

    refactor: ProductCard captures and uses all additional props

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,8 +1,8 @@
 import Rating from './Rating';
 
-function ProductCard({ product }) {
+function ProductCard({ product, ...restProps }) {
   return (
-    <article>
+    <article {...restProps}>
       <img src={product.image} alt={product.title} />
       <h3>{product.title}</h3>
       <p>US ${product.price}</p>
