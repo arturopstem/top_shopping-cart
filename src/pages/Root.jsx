52ad20d2507a97dd4631dc38ec6ccdc340892b46
arturop <67555134+arturopstem@users.noreply.github.com>
2025-06-14 10:21:47 -0500

    feat(layout): Integrate data fetching and state handling in Root
    Root component now:
    - Fetches fakestoreAPI products using useFetch hook
    - Conditionally renders content based on the states returned by
      useFetch hook
    - Add Root.test.jsx which uses MockServiceWorker to test the
      behavior during the data states (loading, success and error)

--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -1,6 +1,20 @@
 import { Outlet } from 'react-router';
 
+import useFetch from '../hooks/useFetch';
+
+const fakeStoreAPI = 'https://fakestoreapi.com/products';
+
 function Root() {
+  const { data, error, loading } = useFetch(fakeStoreAPI);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Oops! Failed to load the products</p>;
+  }
+
   return (
     <>
       <header></header>
---
+++ src/pages/__tests__/Root.test.jsx
@@ -0,0 +1,63 @@
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from '@testing-library/react';
+import { HttpResponse, delay, http } from 'msw';
+import { setupServer } from 'msw/node';
+import {
+  afterAll,
+  afterEach,
+  beforeAll,
+  describe,
+  expect,
+  it,
+  vi,
+} from 'vitest';
+
+import Root from '../Root';
+
+vi.mock('react-router', () => {
+  return { Outlet: () => <div data-testid="Outlet"></div> };
+});
+
+const server = setupServer(
+  http.get('https://fakestoreapi.com/products', () => {
+    return HttpResponse.json([{ id: 1 }, { id: 2 }]);
+  }),
+);
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
+
+describe('Root', () => {
+  it('displays a loading message initially', () => {
+    server.use(
+      http.get('https://fakestoreapi.com/products', async () => {
+        await delay('infinite');
+      }),
+    );
+    render(<Root />);
+    expect(screen.getByText('Loading...')).toBeInTheDocument();
+  });
+
+  it('displays an error message when data could not be loaded', async () => {
+    server.use(
+      http.get('https://fakestoreapi.com/products', () => {
+        return new HttpResponse(null, { status: 404 });
+      }),
+    );
+    render(<Root />);
+    await waitForElementToBeRemoved(() => screen.queryByText('Loading...'));
+    const errorMessage = await screen.findByText(/failed to load/i);
+    expect(errorMessage).toBeInTheDocument();
+  });
+
+  it('renders page content when data is loaded', async () => {
+    render(<Root />);
+    await waitForElementToBeRemoved(() => screen.queryByText('Loading...'));
+    const Outlet = await screen.findByTestId('Outlet');
+    expect(Outlet).toBeInTheDocument();
+  });
+});
